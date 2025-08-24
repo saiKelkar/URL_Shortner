d9500b118b607bdec43c787a6b210599de424ba9
saiKelkar <kelkars1999@gmail.com>
2025-08-24 22:13:24 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,20 @@ export default function App() {
 
   return (
     <>
-      
+      <main>
+        
+        <section>
+          <p>Shorten a long URL</p>
+          <input placeholder="Enter long link here"></input>
+          <button>Shorten URL</button>
+        </section>
+        
+        <section>
+          <p>Shortend URL Link</p>
+          <p></p>
+        </section>
+        
+      </main>
     </>
   )
 }
